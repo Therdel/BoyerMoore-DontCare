3a13479eca7613327acd627c6a1593649f2b7d99
Therdel <14974231+Therdel@users.noreply.github.com>
2020-03-09 17:03:44 +0100

    Quick tests

--- example/main.cpp
+++ example/main.cpp
@@ -3,6 +3,8 @@
 
 #include "BoyerMooreDontCare/BoyerMoore.hpp"
 #include "HeteroArenaAllocator/ArenaAllocator.hpp"
+#include "Signature.hpp"
+#include "BoyerMooreDontCare.hpp"
 
 using namespace std;
 
@@ -41,15 +43,121 @@ auto printMatchIndicatorString(const std::string_view& haystack, const std::vect
 	std::cout << std::endl;
 }
 
+#include <iomanip>
+void test_badCharDontCare(Signature const&signature) {
+	HeapArena arena{ BadCharTableDontCare::arenaSpaceNeeded(signature) };
+	ArenaAllocator arenaAllocator{ arena };
+	BadCharTableDontCare badCharTableDontCare{ arenaAllocator, signature };
+	badCharTableDontCare.printTable();
+
+	size_t mismatchIdx = 4;
+	uint8_t mismatchSymbol = 0xba;
+	std::cout << "Mismatch index=" << mismatchIdx << std::endl;
+	std::cout << "Mismatch symbol=" << std::hex << "0x" << std::uppercase << static_cast<size_t>(mismatchSymbol) << std::resetiosflags(std::ios_base::basefield) << std::endl;
+	std::cout << "Shift: " << badCharTableDontCare.computeShift(mismatchIdx, mismatchSymbol) << std::endl;
+
+	int i = 3;
+}
+
+auto test_matchOrder(Signature const&signature) {
+	auto& pattern = signature.pattern();
+	auto& mask = signature.mask();
+	cout << std::showbase;	// hex numbers with 0x prefix
+
+	cout << "Pattern:\t";
+	for (int i = 0; i < pattern.size(); ++i) {
+		if (mask[i] == SignatureMask::DONT_CARE) {
+			cout << "??\t";
+		}
+		else {
+			cout << std::hex << std::showbase << static_cast<size_t>(pattern[i]) << '\t';
+		}
+	}
+	cout << endl;
+	cout << std::resetiosflags(std::ios_base::hex | std::ios_base::dec)
+		<< std::noshowbase;
+
+	// stdout order
+	auto matchOrderDefault = MatchOrder::compute(signature, MatchOrder::DefaultBoyerMoore);
+	auto matchOrderMaxDontCareDist = MatchOrder::compute(signature, MatchOrder::SortedMaxDontCareDistance);
+	auto printOrder = [&pattern](MatchOrder::MatchOrder_t const& order) {
+		for (int i = 0; i < pattern.size(); ++i) {
+			auto it = std::find(order.begin(), order.end(), i);
+			int orderIndex = std::distance(order.begin(), it);
+			if (it != order.end()) {
+				cout << orderIndex;
+			}
+			cout << '\t';
+		}
+		cout << endl;
+	};
+	cout << "Order: Default\t";
+	printOrder(matchOrderDefault);
+	cout << "Order: MaxDist\t";
+	printOrder(matchOrderMaxDontCareDist);
+}
+
+auto test_GoodSuffixDontCare(Signature const& signature) {
+	HeapArena arena{ GoodSuffixTableDontCare::arenaSpaceNeeded(signature) };
+	ArenaAllocator arenaAllocator{ arena };
+	auto matchOrder = MatchOrder::compute(signature, MatchOrder::SortedMaxDontCareDistance);
+	GoodSuffixTableDontCare table(arenaAllocator, signature, matchOrder);
+
+	auto& pattern = signature.pattern();
+	auto& mask = signature.mask();
+	cout << std::showbase;	// hex numbers with 0x prefix
+
+	cout << "Pattern:\t";
+	for (int i = 0; i < pattern.size(); ++i) {
+		if (mask[i] == SignatureMask::DONT_CARE) {
+			cout << "??\t";
+		}
+		else {
+			cout << std::hex << std::showbase << static_cast<size_t>(pattern[i]) << '\t';
+		}
+	}
+	cout << endl;
+	cout << std::resetiosflags(std::ios_base::hex | std::ios_base::dec)
+		<< std::noshowbase;
+
+	// stdout shifts
+	cout << "GoodTable:\t";
+	for (int i = 0; i < pattern.size(); ++i) {
+		int shift = table.computeShift(i);
+		if (shift > 0) {
+			std::cout << shift;
+		}
+		cout << '\t';
+	}
+	cout << endl;
+	cout << "Aftermatchshift: " << table.afterMatchShift() << endl;
+}
+
+auto test_BoyerMooreDontCare(const Signature& signature) {
+	BoyerMooreDontCare boyerMooreDontCare(signature);
+
+}
+
 int main() {
+	/*
+
+	// _bcde__ef_
+	?? 62 63 64 65 ?? ?? 65 66 ??
+	*/
+	Signature signature("?? 41 4e ");
+	test_matchOrder(signature);
+	test_badCharDontCare(signature);
+	test_GoodSuffixDontCare(signature);
 	// usage example of BoyerMoore search class
-	string_view haystack{ "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed ." };
-	string pattern{ "Lorem ipsum dolor sit ame" };
+	string_view haystack{ "ANPANMAN" };
+	string pattern{ "o" };
 	basic_string_view<uint8_t> haystack_uint8_t{ reinterpret_cast<uint8_t const*>(haystack.data()), haystack.size() };
 	basic_string_view<uint8_t> pattern_uint8_t{ reinterpret_cast<uint8_t*>(pattern.data()), pattern.size() };
 
-	BoyerMoore boyerMoore(pattern_uint8_t);
-	auto matches = boyerMoore.search(haystack_uint8_t);
+	//BoyerMoore boyerMoore(pattern_uint8_t);
+	//auto matches = boyerMoore.search(haystack_uint8_t);
+	BoyerMooreDontCare boyerMooreDontCare(signature);
+	auto matches = boyerMooreDontCare.search(haystack_uint8_t);
 
 	std::cout << "Pattern: '" << pattern << '\'' << std::endl;
 	std::cout << "Haystack:" << std::endl;
